04fc1adada999ed6148221887c3ee19b9eebd4e5
ExponentialWorkload <90570076+Exponential-Workload@users.noreply.github.com>
2023-06-27 17:30:18 +0200

    fix: Fix build script

--- .github/build.sh
+++ .github/build.sh
@@ -1,8 +1,48 @@
 #!/bin/bash
 
+set -e;
+set -x;
+
 cd $1;
 if [ -f "vector.svg" ]; then
-  inkscape -w 8192 -h 8192 vector.svg --export-filename 8192x8192.png;
+  toIco(){
+    magick convert $1x$1.png $1x$1.ico
+    if [ $? -ne "0" ]; then
+      rm $1x$1.ico
+    fi;
+  }
+  toFormat() {
+    inkscape -w $1 -h $1 vector.svg --export-filename $1x$1.$2;
+  }
+  run() {
+    toFormat $1 png;
+    toFormat $1 webp;
+    toFormat $1 gif;
+    toFormat $1 avif;
+    toIco $1;
+  }
+  
+  # ^2
+  run 8192
+  run 4096
+  run 2048
+  run 1024
+  run 512
+  run 256
+  run 128
+  run 64
+  run 32
+  run 16
+  
+  # non ^2
+  run 3072
+  run 1536
+  run 384
+  run 192
+  run 152
+  run 144
+  run 96
+  run 72
 fi;
 ../.github/convert.sh;
 cd ..;
